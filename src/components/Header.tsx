78648a4ed49766a9125e3bbbdb0bd65f9c6cfbe7
kyosu-1 <sho013039@gmail.com>
2023-08-11 22:15:32 +0900

    ヘッダをスタリング

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link to="/portfolio/">About</Link>
-        <Link to="/portfolio/projects">Projects</Link>
-        <Link to="/portfolio/blogs">Blogs</Link>
+    <header className="header">
+      <nav className="navigation">
+        <Link to="/portfolio/" className="nav-link">About</Link>
+        <Link to="/portfolio/projects" className="nav-link">Projects</Link>
+        <Link to="/portfolio/blogs" className="nav-link">Blogs</Link>
       </nav>
     </header>
   );
